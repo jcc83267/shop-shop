b58886779a50d0fe5e70f9adb0639a0406e01077
James Canlas <jamescanlasjr@yahoo.com>
2021-05-13 17:01:06 -0700

    added stripe functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import { StoreProvider } from "./utils/GlobalState";
-
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
@@ -11,6 +10,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
 import OrderHistory from "./pages/OrderHistory";
+import Success from './pages/Success'
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -37,6 +37,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
